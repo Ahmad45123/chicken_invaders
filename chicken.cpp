f4689cc34ade83f3407f1a8cfc849a6fd9e600c7
Ahmed Elwasefi <ahmad.alwasifi@gmail.com>
2022-11-18 00:02:43 +0200

    Added chicken

---
+++ chicken.cpp
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <cmath>
+#include <GL/glut.h>
+
+#include "./util.cpp"
+
+class Chicken
+{
+public:
+    int health = 0;
+    int curX = 5;
+    int curY = 760;
+    bool movingRight = true;
+
+    void tick() {
+        if(movingRight) {
+            curX += 5;
+            if(curX == 780) {
+                movingRight = false;
+            }
+        } else {
+            curX -= 5;
+            if(curX == 5) {
+                movingRight = true;
+            }
+        }
+    }
+
+    void draw() {
+        glPushMatrix();
+
+        glTranslatef(curX, curY, 0);
+        if(!movingRight) {
+            glScalef(-1, 1, 1);
+            glTranslatef(-200, 0, 0);
+        }
+
+        glColor3f(0.41, 0.35, 0.32); // Main body
+        drawCircle(100, 100, 100);
+
+        // draw beak
+        glColor3f(1.00, 0.66, 0.13);
+        drawRect(170, 100, 55, 25);
+
+        // draw hat
+        glColor3f(1.00, 0.21, 0.22);
+        glBegin(GL_POLYGON);
+        glVertex2f(100, 230);
+        glVertex2f(18, 160);
+        glVertex2f(182, 160);
+        glEnd();
+
+        // draw eyes
+        glColor3f(0.19, 0.03, 0.03);
+        drawRect(120, 100, 25, 50);
+
+        glPopMatrix();
+    }
+};
--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 #include <GL/glut.h>
 
 #include "./player.cpp"
+#include "./chicken.cpp"
 
 Player *player;
+Chicken *chicken;
 
 void Draw();
+void Timer(int tmp);
 void KeyDown(int key, int x, int y);
 
 int main(int argc, char **argr)
@@ -18,16 +21,24 @@ int main(int argc, char **argr)
     glutCreateWindow("Chicken Invaders");
     glutDisplayFunc(Draw);
     glutSpecialFunc(KeyDown);
+    glutTimerFunc(30, Timer, 0);
     glutIdleFunc(Draw);
 
     // Setup main objects
     player = new Player();
+    chicken = new Chicken();
 
     glClearColor(COLOR_BACKGROUND, 0);
     gluOrtho2D(0.0, 1000.0, 0.0, 1000.0);
 
     glutMainLoop();
     return 0;
+}
+
+void Timer(int tmp) {
+    chicken->tick();
+    glutPostRedisplay();
+    glutTimerFunc(30, Timer, 0);
 }
 
 void KeyDown(int key, int x, int y) {
@@ -39,6 +50,7 @@ void Draw() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     player->draw();
+    chicken->draw();
 
     glutSwapBuffers();
 }
